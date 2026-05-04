1a96a6ddc2202354a7d36a3df595d22e29847ad3
AKRETCHE Iraihane <12400166@p30216.iutv.univ-paris13.fr>
2026-05-04 14:52:01 +0200

    fix beforeEach

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -7,7 +7,7 @@ import org.springframework.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeEach;
 
 
 
