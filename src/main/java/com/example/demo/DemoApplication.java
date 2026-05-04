1db18765bf9d591f87a99543517ef214feb96a22
AKRETCHE Iraihane <12400166@p30216.iutv.univ-paris13.fr>
2026-05-04 14:12:59 +0200

    fix voiture tests

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,5 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		Voiture voiture = new Voiture('test',1);
+		System.out.println(voiture.toString());
 	}
 }
