eb47f3b52bac68ad29662f496bc5b7450d01c749
AKRETCHE Iraihane <12400166@p30216.iutv.univ-paris13.fr>
2026-05-04 14:04:47 +0200

    add voiture test

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -9,9 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class VoitureTest {
 
+    private final Voiture voitureTest = new Voiture("test",1);
+
     @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    void creerVoiture()
+    {
+        assertEquals(voitureTest.getMarque(),"test");
+        assertEquals(voitureTest.getPrix(),1);
+        assertNotEquals(voitureTest.getId(),1);
     }
 
 }
